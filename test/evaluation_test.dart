f8d9e01b4e9a7336007cf1637dcfd6890db3c331
Minsu Lee <amond@amond.net>
2020-05-29 12:30:47 +0900

    testing: evaluation batch

--- test/evaluation_test.dart
+++ test/evaluation_test.dart
@@ -1,8 +1,7 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/json_object.dart';
-import 'package:built_value/serializer.dart';
+import 'package:flagr/model/evaluation_batch_request.dart';
 import 'package:flagr/model/eval_context.dart';
-import 'package:flagr/api.dart';
 import 'package:mock_web_server/mock_web_server.dart';
 import 'package:test/test.dart';
 
@@ -27,21 +26,49 @@ class EvaluationTest extends BaseTest {
           ..httpCode = 200
       ]);
 
-      EvalContextBuilder()
+      final request = EvalContext((b) => b
         ..enableDebug = true
         ..entityID = 'tset'
         ..flagID = 0
         ..entityType = 'test'
-        ..entityContext = JsonObject({'map': 'test'})
-        ..build();
+        ..entityContext = JsonObject({'map': 'test'}));
 
-      final response =
-          await client.getEvaluationApi().postEvaluation(EvalContext());
+      final response = await client.getEvaluationApi().postEvaluation(request);
       expect(response, isNotNull);
       expect(response.statusCode, 200);
 
       final result = response.data;
       expect(result, isNotNull);
+      expect(result.flagID, 0);
+      expect(result.segmentID, 0);
+      expect(result.variantID, 0);
+      expect(result.flagKey, 'string');
+      expect(result.variantKey, 'string');
+    });
+
+    test('postEvaluationBatch', () async {
+      var json = loadFixture('post_evaluation_batch');
+
+      await enqueue([
+        MockResponse()
+          ..body = json
+          ..httpCode = 200
+      ]);
+
+      final request = EvaluationBatchRequest((b) => b
+        ..enableDebug = true
+        ..entities = ListBuilder([]));
+
+      final response =
+          await client.getEvaluationApi().postEvaluationBatch(request);
+      expect(response, isNotNull);
+      expect(response.statusCode, 200);
+
+      final results = response.data.evaluationResults;
+      expect(results, isNotNull);
+
+      final result = results.first;
+
       expect(result.flagID, 0);
       expect(result.segmentID, 0);
       expect(result.variantID, 0);
